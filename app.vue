0f16910878aa62def63254c7162dc062faf00d57
TB8466 <78532614+TB8466@users.noreply.github.com>
2022-10-11 10:47:18 +0200

    Add files via upload

---
+++ app.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="w-fit xs:w-full">
+    <NavBar/>
+
+    <NuxtPage/>
+    <FooterBarv2/>
+  </div>
+</template>
+
+<script setup>
+  useHead({
+  title: ' 1234567890',
+  // or, instead:
+  // titleTemplate: (title) => `My App - ${title}`,
+  viewport: 'width=device-width, initial-scale=1, maximum-scale=1',
+  charset: 'utf-8'
+})
+
+</script>
+
+<style>
+    
+</style>
